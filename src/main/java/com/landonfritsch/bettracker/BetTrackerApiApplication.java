5b6f27cae804b86a40b2fc1fe054ce426dfdbfe4
Landon Fritsch <lpfritsc@iu.edu>
2026-01-24 20:19:18 -0500

    chore: initialize spring boot project

---
+++ src/main/java/com/landonfritsch/bettracker/BetTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.landonfritsch.bettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/landonfritsch/bettracker/BetTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landonfritsch.bettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
